4ebbb69aaeb5e6561642b540d92026007bf604f6
salmin89 <salmin89@hotmail.com>
2016-08-20 11:34:54 +0200

    Updated to tival.se

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var api_key = 'key-d956cf5597f0751aaac1d4593ad6b990';
-var domain = 'sandboxae45f31018c7420aa1a8f72e463e52c8.mailgun.org';
+var domain = 'tival.se';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 /* GET home page. */
@@ -11,6 +11,7 @@ router.post('/', function(req, res, next) {
   var data = {
     from: 'Tival <no-reply@tival.se>',
     to: 'pe_lias@msn.com',
+    cc: 'salmin89@hotmail.com',
     subject: 'Tival orderbekräftelse',
     text: 'Appartmentnumber: ' + req.body.appartmentnumber +
           ' CustomerOne: ' + req.body.customerOne +
