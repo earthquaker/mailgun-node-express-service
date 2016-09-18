d482f7838f1341dffb999e0933c564b74973364a
Elias Hansen <pe_lias@msn.com>
2016-09-18 16:55:51 +0200

    removed pdfkit

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,11 @@ var router = express.Router();
 var api_key = 'key-d956cf5597f0751aaac1d4593ad6b990';
 var domain = 'tival.se';
 var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-var fs = require('fs');
-var pdfkit = require('pdfkit');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
 
   res.render('index', { title: 'App'});
-
-  // Create PDF
-  doc = new pdfkit;
-  doc.pipe(fs.createWriteStream(req.body.key+'pdf'));
-
-  doc.fontsize(15).text("Test PDF", 50, 50);
-  doc.text("Test Test Test...", {
-    width:410,
-    align:left
-  });
-  doc.end();
 
   // Send mail
   var data = {
